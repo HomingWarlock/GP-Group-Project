6ed84e9666cfcfe3d77387204da3d765c044c725
Michael <michael6.robinson@live.uwe.ac.uk>
2023-05-11 00:37:01 +0100

    Final Slime Combat

--- GP_Unity_File/Assets/Scripts/Player/Combat/CombatHitbox.cs
+++ GP_Unity_File/Assets/Scripts/Player/Combat/CombatHitbox.cs
@@ -10,11 +10,29 @@ public class CombatHitbox : MonoBehaviour
     {
         if (combat_script.combat_attack_delay)
         {
-            if (col.transform.name == "Big Slime" || col.transform.name == "Medium Slime" || col.transform.name == "Small Slime")
+            if (col.transform.name == "Big Slime")
             {
                 if (!combat_script.combat_single_attack)
                 {
                     col.gameObject.GetComponent<SlimeScript>().slime_health -= 1;
+                    combat_script.combat_single_attack = true;
+                }
+            }
+
+            if (col.transform.name == "Medium Slime")
+            {
+                if (!combat_script.combat_single_attack)
+                {
+                    col.gameObject.GetComponent<MediumSlime>().slime_health -= 1;
+                    combat_script.combat_single_attack = true;
+                }
+            }
+
+            if (col.transform.name == "Small Slime")
+            {
+                if (!combat_script.combat_single_attack)
+                {
+                    col.gameObject.GetComponent<SmallSlime>().slime_health -= 1;
                     combat_script.combat_single_attack = true;
                 }
             }
